2bb3e07f53bc591998c9b803e81af841a883ab8f
Renato Lombardero <carlos.lombardero@isobar.com.br>
2017-09-15 17:47:59 -0300

    Init VUE

---
+++ src/components/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data() {
+    return {
+      msg: 'Index',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
